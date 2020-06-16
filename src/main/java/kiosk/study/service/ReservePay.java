56bf4c2cb69eeebc9d63bbcedec0f5c0fe9f9494
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-16 20:31:41 +0900

    예약 페이지(DAO값) 구현

--- src/main/java/kiosk/study/service/ReservePay.java
+++ src/main/java/kiosk/study/service/ReservePay.java
@@ -12,11 +12,11 @@ import kiosk.study.dto.studyDTO;
 
 public class ReservePay {
 	// class파일만 생성함. 실질적으로 sql문 변경은 진행하지 않음; 구분을 위해 둠
-	public StudyRoomDAO dao = new StudyRoomDAO();
+	public ReserveDAO dao = new ReserveDAO();
 	public Map<String, Object> map;
 	public studyDTO dto;
 
-	// #1 사용자 좌석 선택 : 통계치
+	// #1 사용자 좌석 정보 확인
 	public void daySeatSelect(Model model) {
 		map = model.asMap();
 		HttpServletRequest request = (HttpServletRequest) map.get("request");
@@ -25,7 +25,8 @@ public class ReservePay {
 		String seatNum = request.getParameter("seatNum");
 
 		// 좌석 선택 값을 DB에 저장
-		dao.checkStudyRoomInfo(seatNum);
+		dao.checkReserveInfo(seatNum);
+		dao.checkTmrReserveInfo(seatNum);
 	}
 
 	// #2 사용자 입력 값 : 좌석번호, 시간, 가격, 핸드폰을 DTO와 DB에 저장
@@ -35,7 +36,7 @@ public class ReservePay {
 		// model 값 받아오기 위해서 두줄 사용
 
 		// #1 사용자 uniqueUser값 생성
-		dao.RoomPayUser(dto);
+		dao.reservePayUser(dto);
 
 		// #2 테이블 복사 + 시간 값 생성
 		dao.manageCopy(dto);
@@ -48,7 +49,7 @@ public class ReservePay {
 		final String getUniqueUser = dao.getUniqueUser();
 
 		// #3 스터디룸 타임 테이블에 update
-		dao.studyInfoUpdate(dto, getUniqueUser);
+		dao.reserveInfoUpdate(dto, getUniqueUser);
 	}
 
 	public void RoomUser_final(Model model) {
@@ -65,7 +66,7 @@ public class ReservePay {
 		model.addAttribute("dto", dao.daySelectUser(getUniqueUser));
 
 		// #7 좌석 정보를 위해 값 집어넣어줌
-		dao.RoomTotalSeat_Insert();
+		dao.reserveTotalSeat_Insert();
 	}
 
 	// 예약 당일 좌석값
