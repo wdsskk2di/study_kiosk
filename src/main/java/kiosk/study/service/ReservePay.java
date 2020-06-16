f679c246e0fdf38accc907ed8b172ae4ae391223
Jae-Hong-Kim <newfly101@users.noreply.github.com>
2020-06-16 18:18:09 +0900

    ReserveStateService 의 내용 각 Pay에 분할

--- src/main/java/kiosk/study/service/ReservePay.java
+++ src/main/java/kiosk/study/service/ReservePay.java
@@ -6,8 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
 
+import kiosk.study.dao.ReserveDAO;
 import kiosk.study.dao.StudyRoomDAO;
-import kiosk.study.dto.ShowReserveDTO;
 import kiosk.study.dto.studyDTO;
 
 public class ReservePay {
@@ -19,59 +19,80 @@ public class ReservePay {
 	// #1 사용자 좌석 선택 : 통계치
 	public void daySeatSelect(Model model) {
 		map = model.asMap();
-		HttpServletRequest request = (HttpServletRequest)map.get("request");
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
 		// model 값 받아오기 위해서 두줄 사용
 
 		String seatNum = request.getParameter("seatNum");
- 
+
 		// 좌석 선택 값을 DB에 저장
 		dao.checkStudyRoomInfo(seatNum);
 	}
-	
-	
+
 	// #2 사용자 입력 값 : 좌석번호, 시간, 가격, 핸드폰을 DTO와 DB에 저장
 	public void dayRoomSelect(Model model) {
 		map = model.asMap();
-		dto = (studyDTO)map.get("dto");
+		dto = (studyDTO) map.get("dto");
 		// model 값 받아오기 위해서 두줄 사용
-		
+
 		// #1 사용자 uniqueUser값 생성
-		dao.RoomPayUser(dto);	
-		
+		dao.RoomPayUser(dto);
+
 		// #2 테이블 복사 + 시간 값 생성
 		dao.manageCopy(dto);
 	}
-	
+
 	public void RoomUser_unique(Model model) {
 		map = model.asMap();
-		dto = (studyDTO)map.get("dto");
-		
+		dto = (studyDTO) map.get("dto");
+
 		final String getUniqueUser = dao.getUniqueUser();
-		
-		// #3 스터디룸 타임 테이블에 update 
+
+		// #3 스터디룸 타임 테이블에 update
 		dao.studyInfoUpdate(dto, getUniqueUser);
 	}
+
 	public void RoomUser_final(Model model) {
 		map = model.asMap();
-		dto = (studyDTO)map.get("dto");
-		
+		dto = (studyDTO) map.get("dto");
+
 		// #4 결제된 코드 값 반환
 		final String getUniqueUser = dao.getUniqueUser();
-		
-		// #5 study_resultSet 값 삭제 
+
+		// #5 study_resultSet 값 삭제
 		dao.deleteBeforeInfo2();
-		
+
 		// #6 DTO에 사용자에게 보여줄 값들 삽입
-		model.addAttribute("dto",dao.daySelectUser(getUniqueUser));
-		
+		model.addAttribute("dto", dao.daySelectUser(getUniqueUser));
+
 		// #7 좌석 정보를 위해 값 집어넣어줌
 		dao.RoomTotalSeat_Insert();
 	}
 
-	
-	
-	
-	
-	
-	
+	// 예약 당일 좌석값
+	public void reserveToday(Model model) {
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		String seatNum = request.getParameter("seatNum");
+		String title = request.getParameter("title");
+
+		ReserveDAO dao = new ReserveDAO();
+		if (title.equals("r")) {
+			model.addAttribute("reState", dao.checkReserveInfo(seatNum));
+		}
+	}
+
+	// 예약 다음날 좌석값
+	public void reserveNextday(Model model) {
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		String seatNum = request.getParameter("seatNum");
+		String title = request.getParameter("title");
+
+		ReserveDAO dao = new ReserveDAO();
+
+		if (title.equals("r")) {
+			model.addAttribute("reState", dao.checkTmrReserveInfo(seatNum));
+		}
+	}
+
 }
